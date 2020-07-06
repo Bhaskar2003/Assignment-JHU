da004c1bfcef785290973060ce93c53ba4592e7b
Bhaskar Sharma <66830739+Bhaskar2003@users.noreply.github.com>
2020-07-06 12:31:26 +0530

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,37 @@
+## MakeCacheMatrix makes a list with the setters and getters and initialises
+##values of inverse and other free variables.
+
+##cacheSolve computes the inverse or returns the cached value of inverse if 
+##if the inversse has already been computed berfore on the same matrix
+
+
+
+makeCacheMatrix <- function(x = matrix()) {
+      i<-NULL
+      set<-function(y){
+      x<<-y
+      i<<-NULL
+    }
+      get<-function()x
+      settinginverse<-function(inverse) i<<-inverse
+      gettinginverse<-function()i
+      list(set=set,get=get,
+           settinginverse=settinginverse,
+           gettinginverse=gettinginverse)
+  
+  }
+
+
+## Returns the inverse (returns the cached value or computes(if not computed before))
+
+cacheSolve <- function(x,...) {
+        i<-x$gettinginverse()
+        if (!is.null(i)){
+          message('Cached Inverse Available')
+          return(i)
+        }
+        data<-x$get()
+        i<-solve(data,...)
+        x$settinginverse(i)
+        i
+}
